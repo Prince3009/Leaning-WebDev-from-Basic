a6fbf936ef97961636a99998ce078cd27345d0a6
Prince3009 <princechourasia1111@gmail.com>
2024-07-03 22:12:21 +0530

    Fundamentals of JS

---
+++ JavaScript/10LogicalOperator.js
@@ -0,0 +1,21 @@
+/* Logical operator are of three types:
+1. Logical AND - &&
+2. Logical OR - ||
+3. Logical NOT - !
+*/
+
+/* Logical opertors for truthy and falsey values
+Truthy values - "SomeValue"
+Falsey values - "", 0, null, undefined
+*/
+
+const firstName = "Prince";
+const nickName = "";
+
+console.log(firstName && nickName); // false because nickName is empty string
+
+console.log(firstName || nickName); // true because firstName is not empty string
+
+//If both values are falsey values in OR operation then it will return last falsey value
+
+//And in AND operation it will return first falsey value or last truthy value
---
+++ JavaScript/9TernaryOperator.js
@@ -0,0 +1,9 @@
+//condition ? expressionIfTrue : expressionIfFalse
+
+let age = 20;
+
+// Use the ternary operator to check the condition
+let canVote = (age >= 18) ? "You can vote!" : "You cannot vote.";
+
+// Output the result
+console.log(canVote);
